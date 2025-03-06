1ed2d52d18b309733b0e004a9be67125cb4e4b3d
DanielKuhnDeAraujo <danielaprendendo00@gmail.com>
2025-03-06 17:42:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-document.getElementById("menu").innerHTMl="<a href="index.html">Ínicio</a><a href="outro.html">Tarefas.</a><a href="dia.html">Diário</a><a href="ad.html">Anúncios</a>"
+document.getElementById("menu").innerHTMl = "<a href="index.html">Ínicio</a><a href="outro.html">Tarefas.</a><a href="dia.html">Diário</a><a href="ad.html">Anúncios</a>"
